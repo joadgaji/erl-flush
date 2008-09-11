4d4b6ca3d35cfb68d14272fa0b6851b9a785801b
wikiAdrian <wikiAdrian@714164ed-4b54-0410-ae2b-25cda2374f65>
2008-09-11 06:25:51 +0000

    Lexer1 y Parser1 con sus pruebas y Make

--- trunk/lexer1.erl
+++ trunk/lexer1.erl
@@ -10,10 +10,10 @@ stateA([H1, H2|T], Ren, Col, Result)->
 	if
 		(H1 == ${) and (H2 == $%)	->stateB(T, Ren, Col + 2, Result, [H2, H1], Ren, Col, enunciado);
 		(H1 == ${) and (H2 == ${)	->stateB(T, Ren, Col + 2, Result, [H2, H1], Ren, Col, expresion);
-		(H1 == ${) and (H2 == $#)	->stateN(T, Ren, Col + 2, Result, [H2, H1], Ren, Col, comment);
+		%(H1 == ${) and (H2 == $#)	->stateN(T, Ren, Col + 2, Result, [H2, H1], Ren, Col, comment);
 		H1 == 10	->stateJ([H2|T], Ren + 1, 1, Result, [H1], Ren, Col, estatico);
 		(H1 == $$) and (H1 == $$)	->lists:reverse(Result);
-		(H1 == $<) and (H2 == $!)	->stateK(T, Ren, Col + 2 , Result, [H2, H1], Ren, Col, estatico);
+		%(H1 == $<) and (H2 == $!)	->stateK(T, Ren, Col + 2 , Result, [H2, H1], Ren, Col, estatico);
 		true -> stateJ([H2|T], Ren, Col + 1, Result, [H1], Ren, Col, estatico)
 	end.
 	
@@ -35,7 +35,7 @@ stateC([H|T], Ren, Col, Result, Partial, R, C, Type)->
 	
 stateD([H|T], Ren, Col, Result, Partial, R, C, Type)	->
 	if
-		(H == $') and  (Type == comment)	-> stateN(T, Ren, Col, Result, [H|Partial], R, C, Type);
+		%(H == $') and  (Type == comment)	-> stateN(T, Ren, Col, Result, [H|Partial], R, C, Type);
 		H == $"	-> stateC(T, Ren, Col + 1, Result, [H|Partial], R, C, Type);
 		H == $'	-> stateB(T, Ren, Col + 1, Result, [H|Partial], R, C, Type);
 		H == $\\	-> stateF(T, Ren, Col + 1, Result, [H|Partial], R, C, Type);            
@@ -45,7 +45,7 @@ stateD([H|T], Ren, Col, Result, Partial, R, C, Type)	->
 	
 stateE([H|T], Ren, Col, Result, Partial, R, C, Type)	->
 	if
-		(H == $") and  (Type == comment)	-> stateN(T, Ren, Col, Result, [H|Partial], R, C, Type);
+		%(H == $") and  (Type == comment)	-> stateN(T, Ren, Col, Result, [H|Partial], R, C, Type);
 		H == $"	-> stateB(T, Ren, Col + 1, Result, [H|Partial], R, C, Type);
 		H == $'	-> stateG(T, Ren, Col + 1, Result, [H|Partial], R, C, Type);
 		H == $\\	-> stateH(T, Ren, Col + 1, Result, [H|Partial], R, C, Type);
@@ -76,47 +76,47 @@ stateI([H1,H2|T], Ren, Col, Result, Partial, R, C, Type)->
 		(Type == comment) and (H1 == $#) -> stateA(T, Ren, Col + 2, [{Type, R, C, lists:reverse([H2,H1|Partial])}|Result]);
 		(H1 == $$) and (H2 == $$) -> stateA([H1, H2|T], Ren, Col + 2, [{Type, R, C, lists:reverse(Partial)}|Result]);
 		(H1 == ${) and (H2 == ${) -> stateA([H1,H2|T], Ren, Col, [{Type, R, C, lists:reverse(Partial)}|Result]);
-		(H1 == ${) and (H2 == $%) -> stateA([H1,H2|T], Ren, Col, [{Type, R, C, lists:reverse(Partial)}|Result]);
-		(H1 == ${) and (H2 == $#) -> stateA([H1,H2|T], Ren, Col, [{Type, R, C, lists:reverse(Partial)}|Result])
+		(H1 == ${) and (H2 == $%) -> stateA([H1,H2|T], Ren, Col, [{Type, R, C, lists:reverse(Partial)}|Result])
+		%(H1 == ${) and (H2 == $#) -> stateA([H1,H2|T], Ren, Col, [{Type, R, C, lists:reverse(Partial)}|Result])
 	end.
 
 stateJ([H1,H2|T], Ren, Col, Result, Partial, R, C, Type)->
 	if
 		(H1 == ${) and (H2 == $%)	->stateI([H1, H2|T], Ren, Col , Result, Partial, R, C, Type);
 		(H1 == ${) and (H2 == ${)	->stateI([H1, H2|T], Ren, Col , Result, Partial, R, C, Type);
-		(H1 == ${) and (H2 == $#)	->stateI([H1,H2|T], Ren, Col, Result, Partial, R, C, Type);
+		%(H1 == ${) and (H2 == $#)	->stateI([H1,H2|T], Ren, Col, Result, Partial, R, C, Type);
 		(H1 == $$) and (H2 == $$)	->stateI([H1, H2|T], Ren, Col , Result, Partial, R, C, Type);
-		(H1 == $<) and (H2 == $!) -> stateK(T, Ren, Col + 2, Result, [H2, H1|Partial], Ren, Col, Type);
+		%(H1 == $<) and (H2 == $!) -> stateK(T, Ren, Col + 2, Result, [H2, H1|Partial], Ren, Col, Type);
 		H1 == 10	->stateJ([H2|T], Ren + 1, 1, Result, [H1|Partial], R, C, Type);
 		true	->stateJ([H2|T], Ren, Col + 1, Result, [H1|Partial], R, C, Type)
 	end.
 
-stateK([H1,H2|T], Ren, Col, Result, Partial, R , C, Type)->
-	if 
-		(H1 ==$-) and (H2==$-) ->  stateL(T, Ren, Col + 2, Result, [H2, H1|Partial], R, C, Type);
-		H1 == 10	->stateJ([H2|T], Ren + 1, 1, Result, [H1|Partial], R, C, Type);
-		true	-> stateJ([H2|T], Ren, Col + 1, Result, [H1|Partial], R, C, Type)
-		end.
+%stateK([H1,H2|T], Ren, Col, Result, Partial, R , C, Type)->
+%	if 
+%		(H1 ==$-) and (H2==$-) ->  stateL(T, Ren, Col + 2, Result, [H2, H1|Partial], R, C, Type);
+%		H1 == 10	->stateJ([H2|T], Ren + 1, 1, Result, [H1|Partial], R, C, Type);
+%		true	-> stateJ([H2|T], Ren, Col + 1, Result, [H1|Partial], R, C, Type)
+%		end.
 
-stateL([H1, H2|T], Ren, Col, Result, Partial, R, C, Type)->
-	if 
-		(H1 == $-) and (H2 == $-)->	stateM(T, Ren, Col + 2, Result, [H2, H1|Partial], R, C, Type);
-		H1 == 10	->stateL([H2|T], Ren + 1, 1, Result, [H1|Partial], R, C, Type);
-		true	-> stateL([H2|T], Ren, Col + 1, Result, [H1|Partial], R, C, Type)
-	end.
+%stateL([H1, H2|T], Ren, Col, Result, Partial, R, C, Type)->
+%	if 
+%		(H1 == $-) and (H2 == $-)->	stateM(T, Ren, Col + 2, Result, [H2, H1|Partial], R, C, Type);
+%		H1 == 10	->stateL([H2|T], Ren + 1, 1, Result, [H1|Partial], R, C, Type);
+%		true	-> stateL([H2|T], Ren, Col + 1, Result, [H1|Partial], R, C, Type)
+%	end.
 	
-stateM([H|T], Ren, Col, Result, Partial, R, C, Type)->
-	if 
-		(H == $>)->	stateJ(T, Ren, Col + 1, Result, [H|Partial], R, C, Type);
-		H == 10	->stateL(T, Ren + 1, 1, Result, [H|Partial], R, C, Type);
-		true	-> stateL(T, Ren, Col + 1, Result, [H|Partial], R, C, Type)
-	end.
+%stateM([H|T], Ren, Col, Result, Partial, R, C, Type)->
+%	if 
+%		(H == $>)->	stateJ(T, Ren, Col + 1, Result, [H|Partial], R, C, Type);
+%		H == 10	->stateL(T, Ren + 1, 1, Result, [H|Partial], R, C, Type);
+%		true	-> stateL(T, Ren, Col + 1, Result, [H|Partial], R, C, Type)
+%	end.
 	
-stateN([H1,H2|T], Ren, Col, Result, Partial, R, C, Type)->
-	if 
-		(H1 == $#) and (H2 == $}) -> stateI([H1,H2|T], Ren, Col, Result, Partial, R, C, Type);
-		H1 == $"	-> stateE([H2|T], Ren, Col + 1, Result, [H1|Partial], R, C, Type);
-		H1 == $'	-> stateD([H2|T], Ren, Col + 1, Result, [H1|Partial], R, C, Type);
-		H1 == 10 ->  stateN([H2|T], Ren + 1, 1, Result, [H1|Partial], R, C, Type);
-		true	-> stateN([H2|T], Ren, Col + 1, Result, [H1|Partial], R, C, Type)
-	end.
+%stateN([H1,H2|T], Ren, Col, Result, Partial, R, C, Type)->
+%	if 
+%		(H1 == $#) and (H2 == $}) -> stateI([H1,H2|T], Ren, Col, Result, Partial, R, C, Type);
+%		H1 == $"	-> stateE([H2|T], Ren, Col + 1, Result, [H1|Partial], R, C, Type);
+%		H1 == $'	-> stateD([H2|T], Ren, Col + 1, Result, [H1|Partial], R, C, Type);
+%		H1 == 10 ->  stateN([H2|T], Ren + 1, 1, Result, [H1|Partial], R, C, Type);
+%		true	-> stateN([H2|T], Ren, Col + 1, Result, [H1|Partial], R, C, Type)
+%	end.
