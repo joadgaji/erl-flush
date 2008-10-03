b0d5714290da8c394df10d2c0c38f9fa619648b1
wikiAdrian <wikiAdrian@714164ed-4b54-0410-ae2b-25cda2374f65>
2008-10-03 21:22:58 +0000

    cambia el main

--- trunk/main.erl
+++ trunk/main.erl
@@ -5,8 +5,9 @@
 -import(lexer2, [iniciol2/1]).
 
 principal(Input) ->
-	S = lexer1:inicio(parser1:inicio(Input)),
-	stateA(S ++ [{final}], []).
+	R = lexer1:inicio(parser1:inicio(Input)),
+	S = stateA(R ++ [{final}], []),
+	stateB(S ++ [{final}], []).
 	
 stateA([H|T], Result) ->
 	if
@@ -15,3 +16,25 @@ stateA([H|T], Result) ->
 		true -> stateA(T, [H|Result])
 	end.
 	
+stateB([H|T], Result)	->
+	if 
+		final == element(1, H) -> lists:reverse(Result);
+		is_list(H)	-> stateB(T, [stateC(H ++ [{final, 0,0,0}], [])|Result]);
+		true 	-> stateB(T, [H|Result])
+	end.
+	
+stateC([{Type, R, C, A}|T], Partial)	->
+	if
+		Type == simbolo 	-> stateC(T, [{list_to_atom(A), R, C, Type}|Partial]);
+		Type == entero 	-> stateC(T, [{Type, R, C, lists:sum([X- 48||X <- A])}|Partial]);
+		Type == float 	-> stateC(T, [{Type, R, C,lists:sum([X- 48||X <- A])}|Partial]);
+		Type == cadena 	-> stateC(T, [{Type, R, C,lists:concat(A)}|Partial]);
+		Type == final 	-> lists:reverse(Partial);
+		true 	-> stateC(T, [{Type, R, C, A}|Partial])
+	end.
+	
+%convert_to_float([H|T], Partial)	->
+%	if
+%		H == $.	-> covert_to_float(T, [H|Partial]);
+%		H == $$	-> list
+%		true	->covert_to_float(T, [H-48|Partial]);
