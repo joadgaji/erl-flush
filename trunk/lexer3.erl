6b721573f82d86d56060205dbc40bcbf390d375a
wikiAdrian <wikiAdrian@714164ed-4b54-0410-ae2b-25cda2374f65>
2008-11-28 04:46:18 +0000

    Bug en el server

--- trunk/lexer3.erl
+++ trunk/lexer3.erl
@@ -7,20 +7,20 @@
 principal(Input) ->
 	stateA(Input ++ [{final, 0,0,0}], []).
 	
-% Este estado convierte las secuencia de string a su respectivo valor en entero, simbolo, float, cadena o identificador
+%% Este estado convierte las secuencia de string a su respectivo valor en entero, simbolo, float, cadena o identificador
 stateA([{Type, R, C, A}|T], Partial)	->
 	if
 		Type == simbolo	-> stateC(T, Partial, R, C, A);
 		Type == entero 	-> stateA(T, [{Type, R, C, list_to_integer(A)}|Partial]);
 		Type == float 	-> stateA(T, [{Type, R, C, list_to_float("0"++ A)}|Partial]);
-		% El string cuando es leido de un archivo viene con commillas dobles 
+		% El string cuando es leido de un archivo viene con comillas dobles 
 		Type == cadena	-> stateA(T, [{Type, R, C, string:sub_string(lists:concat([A]),2,string:len(A)-1)}|Partial]);
 		Type == identificador	-> stateB([{Type, R, C, A}|T], Partial);
 		Type == final 	-> lists:reverse(Partial);
 		true 	-> throw(invalidsyntax)
 	end.
 
-%Revisa los identificadores or, and, not y xor para convertirlos en atomos 
+%%Revisa los identificadores or, and, not y xor para convertirlos en atomos 
 stateB([{Type, R, C, A}|T], Partial)	->
 	if
 		(A == "or") or (A == "and") or (A == "xor")
@@ -29,7 +29,7 @@ stateB([{Type, R, C, A}|T], Partial)	->
 		true 	-> stateA(T, [{Type, R, C, list_to_atom(A)}|Partial])
 	end.              
 	
-% Revisa la secuencia de simbolos ==, /=, =<, >= ++ para convertirlo a simbolos
+% Revisa la secuencia de simbolos ==, /=, =<, >= ++ para convertirlo a atomos
 stateC([{Type, R2, C2, A2}|T], Partial, R1, C1, A1)	->
 	if
 		(Type /= simbolo ) 	-> stateA([{Type, R2,C2,A2}|T], [{list_to_atom(A1), R1, C1, simbolo}|Partial]);
